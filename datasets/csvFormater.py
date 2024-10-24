ad7b410fc1e393d862732f5972fec92f52a47ee7
Dzanija Ivan <ivan.dzanija222@gmail.com>
2024-10-25 00:07:52 +0200

    Bigger dataset and converted to xlsx, csv and parquet

---
+++ datasets/csvFormater.py
@@ -0,0 +1,37 @@
+import csv
+def main():
+	# fileName = input("Input file: ")
+	# outputName = input("Output file: ")
+	fileName = "notReady.txt"
+	outputName = "public_transport.csv"
+	file = open(fileName, "r")
+	cont = file.read().split("\n")
+	map = {}
+	counts = {}
+	for i in cont:
+		if i == "":
+			continue
+		#i = i.replace("\t", ',')
+		i = i.replace(", ", ',')
+		split = i.split('\t')
+		country = split[1].split(',')[1]
+		if country in map:
+			map[country] += float(split[2])
+			counts[country] += int(1)
+		else:
+			map[country] =  float(split[2])
+			counts[country] = int(1)
+
+
+	corrected = []
+	for r in map.keys():
+		app = [r ,map[r] / counts[r]]
+		print(app)
+		corrected.append(app)
+	with open(outputName, 'w', newline='') as file:
+		writer = csv.writer(file)
+		writer.writerows(corrected)
+	return 0
+
+if __name__ == "__main__":
+	main()
